ac483505bd153d1813f0203f45b69fb0ed97fe8f
Dwi Wjiaya <11dwiwijaya@gmail.com>
2023-12-07 16:12:49 +0700

    chore: labeling footer layout

--- src/components/Layouts/Footer.jsx
+++ src/components/Layouts/Footer.jsx
@@ -4,20 +4,19 @@ const Footer = () => {
             <div className="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
               <div className="mb-2 mb-md-0">
                 ©
-                <script>
-                  document.write(new Date().getFullYear());
-                </script>
+                  {(new Date().getFullYear())}
                 , made with ❤️ by
-                <a href="https://themeselection.com" target="_blank" className="footer-link fw-medium">ThemeSelection</a>
+                <a href="https://themeselection.com" target="_blank" rel='noreferrer' className="footer-link fw-medium">ThemeSelection</a>, Refactor <i style={{color: 'rgb(105, 108, 255)'}} className='bx bx-code-alt'></i>  by
+                <a href="https://github.com/11Dwiwijaya/react-sneat-bootstrap-admin-template" target="_blank" rel='noreferrer' className="footer-link fw-medium">Dwiwijaya</a>
               </div>
               <div className="d-none d-lg-inline-block">
-                <a href="https://themeselection.com/license/" className="footer-link me-4" target="_blank">License</a>
-                <a href="https://themeselection.com/" target="_blank" className="footer-link me-4">More Themes</a>
+                <a href="https://themeselection.com/license/" className="footer-link me-4" target="_blank" rel='noreferrer' >License</a>
+                <a href="https://themeselection.com/" target="_blank" rel='noreferrer' className="footer-link me-4">More Themes</a>
 
                 <a href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/documentation/"
-                  target="_blank" className="footer-link me-4">Documentation</a>
+                  target="_blank" rel='noreferrer' className="footer-link me-4">Documentation</a>
 
-                <a href="https://github.com/themeselection/sneat-html-admin-template-free/issues" target="_blank"
+                <a href="https://github.com/themeselection/sneat-html-admin-template-free/issues" target="_blank" rel='noreferrer'
                   className="footer-link">Support</a>
               </div>
             </div>
