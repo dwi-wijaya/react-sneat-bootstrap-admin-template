6c7a8d0ba687adabf8564b487c6f712940f7efa7
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2023-12-09 18:02:00 +0700

    chore: configuration blank template for some path

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import Layout from "./components/Layouts/Layout";
+import React from "react";
+import { useLocation } from "react-router-dom";
+import Layout from "./Layouts/Layout";
 import AppRoutes from "./router/AppRoutes";
+import { Blank } from "./layouts/Blank";
 
 function App() {
+  const location = useLocation();
+  const isAuthPath = location.pathname.includes("auth") || location.pathname.includes("error") || location.pathname.includes("under-maintenance") | location.pathname.includes("blank");
   return (
-    <Layout>
-      <AppRoutes />
-    </Layout>
+    <>
+      {isAuthPath ? (
+        <Blank>
+          <AppRoutes />
+        </Blank>
+      ) : (
+        <Layout>
+          <AppRoutes />
+        </Layout>
+      )}
+    </>
   );
 }
 
