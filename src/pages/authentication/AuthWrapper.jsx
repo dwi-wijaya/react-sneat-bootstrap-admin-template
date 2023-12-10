b843a2c549d06f7298ff90918076d75209751123
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2023-12-10 09:00:14 +0700

    fix: add container auth wrapper

--- src/pages/authentication/AuthWrapper.jsx
+++ src/pages/authentication/AuthWrapper.jsx
@@ -3,19 +3,21 @@ import { Link } from 'react-router-dom'
 import './page-auth.css'
 export const AuthWrapper = ({ children }) => {
     return (
-        <div className="authentication-wrapper authentication-basic container-p-y">
-            <div className="authentication-inner">
-                <div className="card">
-                    <div className="card-body">
-                        <div className="app-brand justify-content-center">
-                            <Link aria-label='Go to Home Page' to="/" className="app-brand-link gap-2">
-                                <span className="app-brand-logo demo">
-                                    <img src="/assets/img/sneat.svg" alt="sneat-logo" />
-                                </span>
-                                <span className="app-brand-text demo text-body fw-bold">Sneat</span>
-                            </Link>
+        <div className="container-xxl">
+            <div className="authentication-wrapper authentication-basic container-p-y">
+                <div className="authentication-inner">
+                    <div className="card">
+                        <div className="card-body">
+                            <div className="app-brand justify-content-center">
+                                <Link aria-label='Go to Home Page' to="/" className="app-brand-link gap-2">
+                                    <span className="app-brand-logo demo">
+                                        <img src="/assets/img/sneat.svg" alt="sneat-logo" />
+                                    </span>
+                                    <span className="app-brand-text demo text-body fw-bold">Sneat</span>
+                                </Link>
+                            </div>
+                            {children}
                         </div>
-                        {children}
                     </div>
                 </div>
             </div>
